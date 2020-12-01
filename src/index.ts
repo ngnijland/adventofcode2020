b3aa45f3660699c8d1e3ed2475c935c957ae5e25
Niek Nijland <ngnijland@gmail.com>
2020-12-01 08:35:50 +0100

    feat: puzzle solution

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello world');
+const assert = require("assert").strict;
+
+function fixExpenseReport(expenses: number[]): number {
+  for (let i = 0; i < expenses.length; i++) {
+    for (let j = i + 1; j < expenses.length; j++) {
+      if (expenses[i] + expenses[j] === 2020) {
+        return expenses[i] * expenses[j];
+      }
+    }
+  }
+}
+
+assert.strictEqual(fixExpenseReport([1721, 979, 366, 299, 675, 1456]), 514579);
