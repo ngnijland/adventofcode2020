d54e66a8f8d6bbf4e3d6f6c67a563bc4fce18254
Niek Nijland <ngnijland@gmail.com>
2020-12-01 09:11:49 +0100

    feat: solution day 1

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
+const fs = require("fs");
 const assert = require("assert").strict;
+const path = require("path");
 
 function fixExpenseReport(expenses: number[]): number {
   for (let i = 0; i < expenses.length; i++) {
     for (let j = i + 1; j < expenses.length; j++) {
-      if (expenses[i] + expenses[j] === 2020) {
-        return expenses[i] * expenses[j];
+      for (let k = j + 1; k < expenses.length; k++) {
+        if (expenses[i] + expenses[j] + expenses[k] === 2020) {
+          return expenses[i] * expenses[j] * expenses[k];
+        }
       }
     }
   }
 }
 
-assert.strictEqual(fixExpenseReport([1721, 979, 366, 299, 675, 1456]), 514579);
+assert.strictEqual(
+  fixExpenseReport([1721, 979, 366, 299, 675, 1456]),
+  241861950
+);
+
+fs.readFile(
+  path.join(__dirname, "..", "src", "input"),
+  "utf8",
+  function (err: NodeJS.ErrnoException, data: string) {
+    if (err) throw err;
+
+    const input = data.split("\n").map((x) => parseInt(x, 10));
+    console.log(fixExpenseReport(input));
+  }
+);
